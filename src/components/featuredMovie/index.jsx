14dbad7762841ff97b2dd5f66c86a99e8030c458
Lucas Benassi <lucasstreybenassi1999@hotmail.com>
2022-08-05 17:28:23 -0300

    styled Components React

--- src/components/featuredMovie/index.jsx
+++ src/components/featuredMovie/index.jsx
@@ -1,52 +1,77 @@
 /* eslint-disable import/no-anonymous-default-export */
 import React from "react";
-import "../../styled/FeaturedMovie.css"
-import { DescriptionCard, Section } from "./styled";
+import "../../styled/FeaturedMovie.css";
+import {
+  FeaturedAddList,
+  FeaturedButtons,
+  FeaturedDescription,
+  FeaturedHorizontal,
+  FeaturedInfo,
+  FeaturedName,
+  FeaturedPoints,
+  FeaturedSeasons,
+  FeaturedVertical,
+  FeaturedWatchButton,
+  FeaturedYear,
+  FeaturesGenres,
+  Menu,
+  MenuTitle,
+  Section,
+} from "./styled";
 
 export default ({ item }) => {
+  console.log(item);
+  let firstDate = new Date(item.first_air_date);
+  let genres = [];
 
-    console.log (item);
-    let firstDate = new Date (item.first_air_date);
-    let genres =[];
+  for (let i in item.genres) {
+    genres.push(item.genres[i].name);
+  }
 
-    for (let i in item.genres) {
-        genres.push (item.genres[i].name);
-
-    }
-
- // comment
+  // comment
   return (
-    <Section
-       url={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}
-    >
-      <div className="featured--vertical">
-        <div className="featured--horizontal">
-        <div className="featured--name">{item.original_name}</div>
-          <div className="featured--info">
-          <div className="featured--points"> {item.vote_average} pontos</div>
-          <div className="featured--year"> {firstDate.getFullYear()}</div>
-          <div className="featured--seasons"> {item.number_of_seasons} temporada{item.number_of_seasons !== 1? 's' : ''} </div>
-          <DescriptionCard >{limitCaracter(item.overview)}</DescriptionCard>
-          <div className="featured--buttons">
-
-      <a href={`/watch/${item.id}`} className="featured--watchbutton"> ➤ Assistir</a>
-      <a href={`/list/add/${item.id}`}className="featured--mylistbutton">+ Minha Lista</a>
-
-          </div>
-          <div className="features--genres"><strong>Gêneros:</strong> {genres.join(', ')}</div>
-          </div>
-        </div>
-        
-      </div>
+    <Section url={`https://image.tmdb.org/t/p/original${item.backdrop_path}`}>
+      <Menu opa={true}>
+        <MenuTitle>Netflix</MenuTitle>
+      </Menu>
+      <FeaturedVertical>
+        <FeaturedHorizontal>
+          <FeaturedName>{item.original_name}</FeaturedName>
+          <FeaturedInfo>
+            <FeaturedPoints> {item.vote_average} pontos</FeaturedPoints>
+            <FeaturedYear> {firstDate.getFullYear()}</FeaturedYear>
+            <FeaturedSeasons>
+              {" "}
+              {item.number_of_seasons} temporada
+              {item.number_of_seasons !== 1 ? "s" : ""}{" "}
+            </FeaturedSeasons>
+            <FeaturedDescription>
+              {limitCaracter(item.overview)}
+            </FeaturedDescription>
+            <FeaturedButtons>
+              <FeaturedWatchButton href={`/watch/${item.id}`}>
+                {" "}
+                ➤ Assistir
+              </FeaturedWatchButton>
+              <FeaturedAddList href={`/list/add/${item.id}`}>
+                + Minha Lista
+              </FeaturedAddList>
+            </FeaturedButtons>
+            <FeaturesGenres>
+              <strong>Gêneros:</strong> {genres.join(", ")}
+            </FeaturesGenres>
+          </FeaturedInfo>
+        </FeaturedHorizontal>
+      </FeaturedVertical>
     </Section>
   );
 };
 
-
 const limitCaracter = (value) => {
   console.log(value.length);
-  if(value.length > 350) {
-    return `${value.slice(0, 351)}...`
-
-  }  else {return value}
-}
+  if (value.length > 350) {
+    return `${value.slice(0, 351)}...`;
+  } else {
+    return value;
+  }
+};
--- src/components/featuredMovie/styled.js
+++ src/components/featuredMovie/styled.js
@@ -1,4 +1,27 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+export const Menu = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: black;
+  height: 35px;
+  opacity: ${props => props.opa? 1 : 0.5};
+  
+`;
+
+export const MenuTitle = styled.div`
+  opacity: 1 !important;
+  
+margin-top: 5px;
+  color: #e50914;
+  font-size: 25px;
+  margin-left: 30px;
+  font-weight: bold;
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+`;
 
 export const Section = styled.section`
   height: 100vh;
@@ -7,11 +30,94 @@ export const Section = styled.section`
   background-image: url(${(props) => props.url});
 `;
 
-export const DescriptionCard = styled.div`
-  margin-top: 15px;
+export const FeaturedDescription = styled.div`
+  
   font-size: 20px;
   color: #999;
   max-width: 45%;
-  height: 170px;
-  
+  height: 165px;
+  margin:15px;
+`;
+export const FeaturedVertical = styled.div`
+  width: inherit;
+  height: inherit;
+  background: linear-gradient(to top, #111 30%, transparent 70%);
+`;
+export const FeaturedHorizontal = styled.div`
+  width: inherit;
+  height: inherit;
+  background: linear-gradient(to right, #111 25%, transparent 75%);
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding-left: 30px;
+  padding-bottom: 150px;
+  padding-top: 70px;
+`;
+
+export const FeaturedName = styled.div`
+  font-size: 60px;
+  font-weight: bold;
+`;
+
+const Pattern = css`
+  font-size: 18px;
+  font-weight: bold;
+  margin-top: 15px;
+`;
+
+export const FeaturedPoints = styled.div`
+  ${Pattern}
+  color: #46d369;
+  margin:0 0 7px 0; 
+`;
+
+export const FeaturedInfo = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  margin:15px 0 5px 0;
+`;
+
+export const FeaturedYear = styled.div`
+  font-weight: bold;
+  font-size: 18px;
+  margin:0 0 3px 0;
+`;
+export const FeaturedSeasons = styled.div``;
+
+export const FeaturedButtons = styled.div`
+  padding-top: 15px; ;
+`;
+
+export const FeaturesGenres = styled.div`
+  font-weight: bold;
+  margin-top: 15px;
+  font-size: 18px;
+  color: #999;
+`;
+
+const PatternButton = css`
+  display: inline-block;
+  font-size: 20px;
+  font-weight: bold;
+  padding: 15px 20px;
+  border-radius: 5px;
+  text-decoration: none;
+  margin-right: 10px;
+  opacity: 1;
+  transition: all ease 0.2s;
+`;
+
+export const FeaturedWatchButton = styled.a`
+  ${PatternButton}
+  background-color: #fff;
+  color: #000;
+  :hover {
+opacity: 0.6;
+  }
+`;
+
+export const FeaturedAddList = styled(FeaturedWatchButton)`
+background-color: #333;
+color: #fff;
 `;
